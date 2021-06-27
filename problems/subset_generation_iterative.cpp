d06447cf854a334f8f7b94a1a4624ad9b49db2b8
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-06-27 13:45:50 +0530

    Create subset_generation_iterative.cpp

---
+++ problems/subset_generation_iterative.cpp
@@ -0,0 +1,55 @@
+/*
+    Generating subset using bitwise operations.
+    
+    for ex: s = "abc"
+            number of subsets possible = 2^3             
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubstring(string s){
+
+	int n = s.length();
+	int max = (1<<n) -1;
+
+	for(int i= 0;i<=max;i++){
+
+		int temp = i;
+		string ans = "";
+		for(int j=s.length()-1;j>=0;j--){
+
+			int rem = temp % 2;
+
+			if(rem == 0){
+				// cout<<" ";
+				ans += " ";
+			}
+			else
+			{
+				// cout<<s[j]<<" ";
+				ans = s[j] + ans;
+			}
+
+			temp = temp>>1;
+
+		}
+
+		cout<<ans<<endl;
+	}
+
+
+
+}
+int main(){
+
+// 	 #ifndef ONLINE_JUDGE
+// 		freopen("input.txt", "r", stdin);
+// 		freopen("output.txt", "w", stdout);
+// 	#endif
+
+	string s = "abc";
+	printSubstring(s);	
+
+}
