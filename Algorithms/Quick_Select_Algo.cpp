469fce6021f209e02eb538e3e805dee0aea3aa4b
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-07-16 21:13:54 +0530

    Update Quick_Select_Algo.cpp

--- Algorithms/Quick_Select_Algo.cpp
+++ Algorithms/Quick_Select_Algo.cpp
@@ -5,57 +5,60 @@
     It use concept of partition from QUick Sort algorithm
 */
 
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int partition(vector<int>&arr,int start,int end){
 
-int partition(vector<int> &arr,int low,int high)  //same logic as Quick Sort
-{
-	int pivot = arr[high],pIndex = low;
+	int pivot = arr[end];
+	int pIndex = start;
 
-	for(int i=low;i<=high-1;i++)
-	{
-		if(arr[i]<=pivot)
-		{
-			swap(arr[i],arr[pIndex]);
-			pIndex++;
+	for(int i= start;i<end;i++){
+
+		if(arr[i] <= pivot){
+			swap(arr[i],arr[pIndex++]);
 		}
 	}
 
-	swap(arr[pIndex],arr[high]);
+	swap(arr[end],arr[pIndex]);
 
 	return pIndex;
 }
 
-void quickSelect(vector<int>& arr,int low,int high,int k)
-{
-	if(low <high){
 
-	int pIndex = partition(arr,low,high);
+void quickSort(vector<int>& arr,int start,int end,int k){
 
-	if(pIndex < k)
-	{
-		quickSelect(arr,pIndex+1,high,k);
+	int pIndex = partition(arr,start,end);
+	// cout<<pIndex<<endl;
+
+	if(pIndex > k){
+		quickSort(arr,start,pIndex-1,k);
 	}
-	else if(pIndex > k)
-	{
-		quickSelect(arr,low,pIndex-1,k);
-	}
+	else if(pIndex < k)
+		quickSort(arr,pIndex + 1,end,k);
 	else
-	{
 		cout<<arr[pIndex]<<endl;
-	} 
-}
 }
 
-int main()
-{
-	vector<int> arr = {1,9,4,5,7,6,8,2,3};
-	int k = 8;
+int main(){
 
-	quickSelect(arr,0,8,k-1);
+// 	 #ifndef ONLINE_JUDGE
+// 		freopen("input.txt", "r", stdin);
+// 		freopen("output.txt", "w", stdout);
+// 	#endif
 
-	for(int i: arr){
-		cout<<i<<" ";
-	}
+    vector<int> arr = {1,9,-1,0,222,69,25};
+    // after sorting arr = -1 0 1 9 25 69 222
+
+    int start = 0;
+    int end = arr.size() - 1;
+    int k = 1;
+    quickSort(arr,start,end,k-1);
+
+    for(auto i: arr){
+    	cout<<i<<" ";
+    }
+
+	
 }
